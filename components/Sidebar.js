96e7cb252355cebbd5ed9512d3f6fa57c173166f
David Clarke <drclarke99@gmail.com>
2023-05-17 17:34:52 -0700

    created post db and sendTweet

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,5 @@
 import { auth } from "@/firebase";
+import { closeLoginModal, closeSignupModal } from "@/redux/modalSlice";
 import { signOutUser } from "@/redux/userSlice";
 import {HomeIcon, 
         HashtagIcon,
@@ -23,6 +24,8 @@ export default function Sidebar() {
     async function handleSignOut() {
         await signOut(auth);
         dispatch(signOutUser());
+        dispatch(closeSignupModal());
+        dispatch(closeLoginModal());
     }
 
     return (
